95be98a3193e29cde791c791293532561801d606
giorgimaisuradze21 <maisuradzegio40@gmail.com>
2025-08-16 19:36:39 +0400

    first commit

---
+++ Level81/Homework/script.js
@@ -0,0 +1,19 @@
+// const person = {
+//     name: "გიორგი",
+//     age: 25,
+//     city: "თბილისი"
+// };
+
+// for (let key in person) {
+//     console.log(key + ": " + person[key]);
+// }
+
+
+// const numbers = [5, 10, 15, 20];
+
+// let sum = 0;
+// for (let num of numbers) {
+//     sum += num;
+// }
+
+// console.log(sum);
