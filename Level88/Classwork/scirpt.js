8b48cb16c27129a3603d9019f1f086a350b04abe
giorgimaisuradze21 <maisuradzegio40@gmail.com>
2025-08-23 19:16:20 +0400

    first commit

---
+++ Level88/Classwork/scirpt.js
@@ -0,0 +1,32 @@
+// let a = 10, b = 20;
+// [a, b] = [b, a];
+// console.log(a);
+// console.log(b);
+
+// const arr = [1, [2, 3], 4];
+// const [x, [y, z], w] = arr;
+// console.log(x);
+// console.log(y);
+// console.log(z);
+// console.log(w);
+
+// const car = { brand: "Toyota", year: 2022 };
+// const { brand, year } = car;
+// console.log(brand);
+// console.log(year);
+
+// const user = {
+//   name: "Alice",
+//   address: {
+//     city: "Tbilisi",
+//     zip: 12345
+//   }
+// };
+// const { address: { city, zip } } = user;
+// console.log(city);
+// console.log(zip);
+
+// const book = { title: "JS Guide", pages: 300 };
+// const { title: bookTitle, pages: totalPages } = book;
+// console.log(bookTitle);
+// console.log(totalPages);
---
+++ Level88/Homework/script.js
@@ -0,0 +1,22 @@
+// const person = { name: "Alice", age: 30, city: "New York" };
+// const { name: userName, city: userCity } = person;
+// console.log(userName);
+// console.log(userCity);
+
+// const student = {
+//   name: "Bob",
+//   age: 20,
+//   grades: {
+//     math: 90,
+//     science: 85,
+//     english: 92,
+//   },
+// };
+// const { grades: { math, english } } = student;
+// console.log(math);
+// console.log(english);
+
+// const numbers = [1, 2, 3, 4, 5];
+// const [first, , third] = numbers;
+// console.log(first);
+// console.log(third);
